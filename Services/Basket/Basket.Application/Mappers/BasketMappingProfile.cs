45a33fd74b2f72ae306a220d5448cb391ca8182e
Jagannath Ojha <ojha.jagann@gmail.com>
2025-01-01 17:09:42 +0545

    basket commands and queries

---
+++ Services/Basket/Basket.Application/Mappers/BasketMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
+//using EventBus.Messages.Common;
+//using EventBus.Messages.Events;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Basket.Application.Mappers
+{
+    public class BasketMappingProfile : Profile
+    {
+        public BasketMappingProfile()
+        {
+            CreateMap<ShoppingCart, ShoppingCartResponse>().ReverseMap();
+            CreateMap<ShoppingCartItem, ShoppingCartItemResponse>().ReverseMap();
+            //CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+            //CreateMap<BasketCheckoutV2, BasketCheckoutEventV2>().ReverseMap();
+        }
+    }
+}
