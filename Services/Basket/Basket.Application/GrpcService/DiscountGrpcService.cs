4a1336a85b20b1598450d6a9511c2832cb81d8a9
Jagannath Ojha <ojha.jagann@gmail.com>
2025-01-02 10:11:49 +0545

    setup discount grpc on basket service

--- Services/Basket/Basket.Application/GrpcService/DiscountGrpcService.cs
+++ Services/Basket/Basket.Application/GrpcService/DiscountGrpcService.cs
@@ -1,20 +1,20 @@
-﻿//using Discount.Grpc.Protos;
+﻿using Discount.Grpc.Protos;
 
-//namespace Basket.Application.GrpcService
-//{
-//    public class DiscountGrpcService
-//    {
-//        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoServiceClient;
+namespace Basket.Application.GrpcService
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoServiceClient;
 
-//        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
-//        {
-//            _discountProtoServiceClient = discountProtoServiceClient;
-//        }
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
+        {
+            _discountProtoServiceClient = discountProtoServiceClient;
+        }
 
-//        public async Task<CouponModel> GetDiscount(string productName)
-//        {
-//            var discountRequest = new GetDiscountRequest { ProductName = productName };
-//            return await _discountProtoServiceClient.GetDiscountAsync(discountRequest);
-//        }
-//    }
-//}
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            return await _discountProtoServiceClient.GetDiscountAsync(discountRequest);
+        }
+    }
+}
