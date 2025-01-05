cbe496410d783a256570dc3496f1f4ac12a47014
Jagannath Ojha <ojha.jagann@gmail.com>
2025-01-05 10:28:22 +0545

    ocelot api gateway

---
+++ ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -0,0 +1,45 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//ocelot configuration
+builder.Host.ConfigureAppConfiguration((env, config) =>
+{
+    config.AddJsonFile($"ocelot.{env.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseEndpoints(endponits =>
+{
+    endponits.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello Ocelot");
+    });
+});
+
+app.UseOcelot();
+
+app.Run();
