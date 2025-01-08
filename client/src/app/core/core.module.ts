e7098ca3a0794c879835d6efd5ae4cf0cdf8eb0e
Jagannath Ojha <ojha.jagann@gmail.com>
2025-01-08 22:54:39 +0545

    header and breadcrumb

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,17 +5,27 @@ import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { UnAuthenticatedComponent } from './un-authenticated/un-authenticated.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, NotFoundComponent, ServerErrorComponent, UnAuthenticatedComponent],
+  declarations: [
+    NavbarComponent,
+    NotFoundComponent,
+    ServerErrorComponent, 
+    UnAuthenticatedComponent, 
+    HeaderComponent,
+  ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BreadcrumbModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ]
 })
 export class CoreModule { }
