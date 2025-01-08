af7f14f85c0aed9c7a5c47856bc4d80a31ee4454
Jagannath Ojha <ojha.jagann@gmail.com>
2025-01-08 22:19:52 +0545

    interceptor and error handling

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { UnAuthenticatedComponent } from './un-authenticated/un-authenticated.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, NotFoundComponent, ServerErrorComponent, UnAuthenticatedComponent],
   imports: [
     CommonModule,
     RouterModule
